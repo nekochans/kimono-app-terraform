972d413b1bf99ca71554ac9eaba31740b936698a
kobayashi-m42 <m42kobayashi@gmail.com>
2020-05-23 00:18:28 +0900

    :bug: #2 NATインスタンス用のAMIに変更

--- providers/aws/environments/stg/10-network/variables.tf
+++ providers/aws/environments/stg/10-network/variables.tf
@@ -22,7 +22,7 @@ locals {
     "10.1.12.0/24"
   ]
 
-  nat_instance_ami         = "ami-0af1df87db7b650f4"
+  nat_instance_ami         = "ami-016892bae608cf892"
   nat_instance_type        = "t2.micro"
   nat_instance_volume_type = "gp2"
   nat_instance_volume_size = "30"
