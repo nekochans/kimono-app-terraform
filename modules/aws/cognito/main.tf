953d09e91cd71627693b867551ea4e6d7d6b2d25
kobayashi-m42 <m42kobayashi@gmail.com>
2020-06-02 00:59:08 +0900

    :sparkles: #3 アプリクライアントを追加

--- modules/aws/cognito/main.tf
+++ modules/aws/cognito/main.tf
@@ -35,3 +35,12 @@ resource "aws_cognito_user_pool" "pool" {
     }
   }
 }
+
+resource "aws_cognito_user_pool_client" "client" {
+  name                          = var.user_pool_name
+  user_pool_id                  = aws_cognito_user_pool.pool.id
+  generate_secret               = false
+  prevent_user_existence_errors = "ENABLED"
+  refresh_token_validity        = 30
+  explicit_auth_flows           = ["ALLOW_ADMIN_USER_PASSWORD_AUTH", "ALLOW_REFRESH_TOKEN_AUTH"]
+}
