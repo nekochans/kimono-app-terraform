b968cc73d2cc61b8038456a054366c55e9a42b3e
kobayashi-m42 <m42kobayashi@gmail.com>
2020-08-08 18:18:51 +0900

    :sparkles: #23 Authorization code grantフローでカスタムスコープを含んだアクセストークンを取得できるようにCognitoを修正

--- modules/aws/cognito/variables.tf
+++ modules/aws/cognito/variables.tf
@@ -2,6 +2,26 @@ variable "user_pool_name" {
   type = string
 }
 
+variable "user_pool_domain" {
+  type = string
+}
+
+variable "resource_server_name" {
+  type = string
+}
+
+variable "resource_server_identifier" {
+  type = string
+}
+
 variable "ses_email_identity_arn" {
   type = string
+}
+
+variable "allowed_oauth_scopes" {
+  type = list(string)
+}
+
+variable "callback_urls" {
+  type = list(string)
 }
--- providers/aws/environments/stg/14-cognito/main.tf
+++ providers/aws/environments/stg/14-cognito/main.tf
@@ -1,6 +1,11 @@
 module "api" {
   source = "../../../../../modules/aws/cognito"
 
-  user_pool_name         = local.user_pool_name
-  ses_email_identity_arn = data.terraform_remote_state.ses.outputs.ses_email_identity_arn
+  user_pool_name             = local.user_pool_name
+  user_pool_domain           = local.user_pool_domain
+  resource_server_name       = local.resource_server_name
+  resource_server_identifier = local.resource_server_identifier
+  ses_email_identity_arn     = data.terraform_remote_state.ses.outputs.ses_email_identity_arn
+  allowed_oauth_scopes       = local.allowed_oauth_scopes
+  callback_urls              = local.callback_urls
 }
