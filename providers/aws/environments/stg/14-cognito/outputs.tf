68f5393abbe21b86bbd67ba778f551c69fd39ef2
keitakn <keita.koga.work@gmail.com>
2021-01-09 23:56:10 +0900

    :pencil2: #43 moduleの名前が実態と違っていたので修正

--- providers/aws/environments/stg/14-cognito/outputs.tf
+++ providers/aws/environments/stg/14-cognito/outputs.tf
@@ -1,11 +1,11 @@
 output "cognito_user_pool_endpoint" {
-  value = module.api.cognito_user_pool_endpoint
+  value = module.cognito.cognito_user_pool_endpoint
 }
 
 output "cognito_user_pool_arn" {
-  value = module.api.cognito_user_pool_arn
+  value = module.cognito.cognito_user_pool_arn
 }
 
 output "kimono_app_frontend_client_id" {
-  value = module.api.kimono_app_frontend_client_id
+  value = module.cognito.kimono_app_frontend_client_id
 }
