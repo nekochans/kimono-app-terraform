91f886b6d8c9b2e9b25871304e1638440dc4222c
keitakn <keita.koga.work@gmail.com>
2021-01-09 18:43:33 +0900

    :recycle: #43 CognitoのClientIDはoutputから参照したほうが間違いが起きにくいので、そのように修正

--- modules/aws/cognito/outputs.tf
+++ modules/aws/cognito/outputs.tf
@@ -5,3 +5,7 @@ output "cognito_user_pool_endpoint" {
 output "cognito_user_pool_arn" {
   value = aws_cognito_user_pool.pool.arn
 }
+
+output "kimono_app_frontend_client_id" {
+  value = aws_cognito_user_pool_client.client.id
+}
--- providers/aws/environments/stg/14-cognito/outputs.tf
+++ providers/aws/environments/stg/14-cognito/outputs.tf
@@ -5,3 +5,7 @@ output "cognito_user_pool_endpoint" {
 output "cognito_user_pool_arn" {
   value = module.api.cognito_user_pool_arn
 }
+
+output "kimono_app_frontend_client_id" {
+  value = module.api.kimono_app_frontend_client_id
+}
